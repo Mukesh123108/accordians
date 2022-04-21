3e6d4686c2420f5933fefce74df9db0e792e8927
mukesh <mukeshmaurya9460@gmail.com>
2022-04-21 07:17:21 +0530

    Updates

---
+++ precache-manifest.f5e1fd1b721ace0ebfa5eef9b4d37208.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "19a75b015a4b1c0d407a",
+    "url": "/accordians/static/css/main.732a80f6.chunk.css"
+  },
+  {
+    "revision": "19a75b015a4b1c0d407a",
+    "url": "/accordians/static/js/main.19a75b01.chunk.js"
+  },
+  {
+    "revision": "72ff4d8d2a3bc6f3ce0c",
+    "url": "/accordians/static/js/1.72ff4d8d.chunk.js"
+  },
+  {
+    "revision": "84ca9d00bdf123627fb0",
+    "url": "/accordians/static/js/runtime~main.84ca9d00.js"
+  },
+  {
+    "revision": "e0e292fd5d98ed982dfa3bbee560ce21",
+    "url": "/accordians/index.html"
+  }
+];
